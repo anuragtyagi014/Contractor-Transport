80d19d706c1ac0383e5111409771798bc5122bd8
Anurag <anuragtyagi014@gmail.com>
2024-05-08 18:27:48 +0530

    Fixed issue

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The base configuration for WordPress
  *
@@ -19,24 +20,25 @@
  * @package WordPress
  */
 
+
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'dbiheslglqpdug' );
+define('DB_NAME', 'dbiheslglqpdug');
 
 /** Database username */
-define( 'DB_USER', 'u8hnmdaxqui0e' );
+define('DB_USER', 'u8hnmdaxqui0e');
 
 /** Database password */
-define( 'DB_PASSWORD', 'mgolc1omxzhb' );
+define('DB_PASSWORD', 'mgolc1omxzhb');
 
 /** Database hostname */
-define( 'DB_HOST', '127.0.0.1' );
+define('DB_HOST', '127.0.0.1');
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define('DB_CHARSET', 'utf8');
 
 /** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication unique keys and salts.
@@ -49,15 +51,15 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',          'u<^+-}+TYoFREj1%p_Ejkopc3vQY-5k%7{=;EFyXF$##pGBm>9}t?uoh$<JYHEL8' );
-define( 'SECURE_AUTH_KEY',   'zVEF:L@(O$7 #bh!D2<BP_4w/dmxQ&vn)drt)^^9IlI-,`,lNL4m7-Vu5j`uF5Lo' );
-define( 'LOGGED_IN_KEY',     'kU8us@IE0JfXq)}f-yl5#>}w9<qt;_<h6.>w,hpz.U#`j[#62q</P<qNHLpeq8)-' );
-define( 'NONCE_KEY',         'BMinO#Wt:R-Kc$EYAwyXMWf(Xz9=jb5]C9!1.u* t-K<;[U[Er$edz`1k7?Emc:.' );
-define( 'AUTH_SALT',         '1A]}LrsEPpgbCLLlT}p]]LI@`*Dr`y:X(.$<brxO,RZ]6OY6WQ5bF/_:s_/L4p)N' );
-define( 'SECURE_AUTH_SALT',  'GJMi}rQGH(v[W]@D3=/&yf7W}W`;d!1BJt%7BDK%}Dnwk&9<qR$XTppw31=Xk{(/' );
-define( 'LOGGED_IN_SALT',    'F% j)lr9$S.Gz#QGE.T2xl|DWubi(!M%(|Ue(J]BBQc6y2pWrw,!e.w(VRRPk+8Z' );
-define( 'NONCE_SALT',        'oBTs&654dnIH.M% =M[ek9<Bx-%&.;Z4pu3<C7v2{9`OtLzdW1dy) 7ZV)j-:kwm' );
-define( 'WP_CACHE_KEY_SALT', 'Lb`A<1y#a4P4b>AcOvE$R4}OFMW{Ls.EV:5z}w$@CR6v?*{`G[IY5?@,ELO}3n21' );
+define('AUTH_KEY',          'u<^+-}+TYoFREj1%p_Ejkopc3vQY-5k%7{=;EFyXF$##pGBm>9}t?uoh$<JYHEL8');
+define('SECURE_AUTH_KEY',   'zVEF:L@(O$7 #bh!D2<BP_4w/dmxQ&vn)drt)^^9IlI-,`,lNL4m7-Vu5j`uF5Lo');
+define('LOGGED_IN_KEY',     'kU8us@IE0JfXq)}f-yl5#>}w9<qt;_<h6.>w,hpz.U#`j[#62q</P<qNHLpeq8)-');
+define('NONCE_KEY',         'BMinO#Wt:R-Kc$EYAwyXMWf(Xz9=jb5]C9!1.u* t-K<;[U[Er$edz`1k7?Emc:.');
+define('AUTH_SALT',         '1A]}LrsEPpgbCLLlT}p]]LI@`*Dr`y:X(.$<brxO,RZ]6OY6WQ5bF/_:s_/L4p)N');
+define('SECURE_AUTH_SALT',  'GJMi}rQGH(v[W]@D3=/&yf7W}W`;d!1BJt%7BDK%}Dnwk&9<qR$XTppw31=Xk{(/');
+define('LOGGED_IN_SALT',    'F% j)lr9$S.Gz#QGE.T2xl|DWubi(!M%(|Ue(J]BBQc6y2pWrw,!e.w(VRRPk+8Z');
+define('NONCE_SALT',        'oBTs&654dnIH.M% =M[ek9<Bx-%&.;Z4pu3<C7v2{9`OtLzdW1dy) 7ZV)j-:kwm');
+define('WP_CACHE_KEY_SALT', 'Lb`A<1y#a4P4b>AcOvE$R4}OFMW{Ls.EV:5z}w$@CR6v?*{`G[IY5?@,ELO}3n21');
 
 
 /**#@-*/
@@ -87,15 +89,15 @@ $table_prefix = 'dmm_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-if ( ! defined( 'WP_DEBUG' ) ) {
-	define( 'WP_DEBUG', false );
+if (!defined('WP_DEBUG')) {
+	define('WP_DEBUG', false);
 }
 
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+if (!defined('ABSPATH')) {
+	define('ABSPATH', __DIR__ . '/');
 }
 
 /** Sets up WordPress vars and included files. */
--- wp-content/themes/business-club/archive.php
+++ wp-content/themes/business-club/archive.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying archive pages.
  *
@@ -9,48 +10,49 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<?php if (have_posts()) : ?>
 
 			<header class="page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				the_archive_title('<h1 class="page-title">', '</h1>');
+				the_archive_description('<div class="taxonomy-description">', '</div>');
 				?>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php while (have_posts()) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
+				<?php get_template_part('template-parts/content'); ?>
 
 			<?php endwhile; ?>
 
-		<?php
-		/**
-		 * Hook - business_club_action_posts_navigation.
-		 *
-		 * @hooked: business_club_custom_posts_navigation - 10
-		 */
-		do_action( 'business_club_action_posts_navigation' ); ?>
+			<?php
+			/**
+			 * Hook - business_club_action_posts_navigation.
+			 *
+			 * @hooked: business_club_custom_posts_navigation - 10
+			 */
+			do_action('business_club_action_posts_navigation'); ?>
 
 		<?php else : ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+			<?php get_template_part('template-parts/content', 'none'); ?>
 
 		<?php endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
-	/**
-	 * Hook - business_club_action_sidebar.
-	 *
-	 * @hooked: business_club_add_sidebar - 10
-	 */
-	do_action( 'business_club_action_sidebar' );
+/**
+ * Hook - business_club_action_sidebar.
+ *
+ * @hooked: business_club_add_sidebar - 10
+ */
+do_action('business_club_action_sidebar');
 ?>
 <?php get_footer(); ?>
